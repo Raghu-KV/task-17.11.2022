4ab1bebb4f680bc21bd47317876c3e4742770714
Raghu-KV <raghunandanv19@gmail.com>
2022-11-21 14:26:08 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
+import PriceCard from "./components/PriceCard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row my-height d-flex align-items-center">
+        <PriceCard
+          content={[
+            "Single User",
+            "5GB Storage",
+            "Unlimited Public Projects",
+            "Community Access",
+          ]}
+          plan={"FREE"}
+          price={"$0"}
+          notValid={[
+            "Unlimited Private Projects",
+            "Dedicated Phone Support",
+            "Free Subdomain",
+            "Monthly Status Reports",
+          ]}
+        />
+        <PriceCard
+          content={[
+            "5 Users",
+            "50GB Storage",
+            "Unlimited Public Projects",
+            "Community Access",
+            "Unlimited Private Projects",
+            "Dedicated Phone Support",
+            "Free Subdomain",
+          ]}
+          plan={"PLUS"}
+          price={"$9"}
+          notValid={["Monthly Status Reports"]}
+        />
+        <PriceCard
+          content={[
+            "Unlimited Users",
+            "150GB Storage",
+            "Unlimited Public Projects",
+            "Community Access",
+            "Unlimited Private Projects",
+            "Dedicated Phone Support",
+            "Unlimited Free Subdomains",
+            "Monthly Status Reports",
+          ]}
+          plan={"PRO"}
+          price={"$49"}
+          notValid={[]}
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
+// ✖✔
