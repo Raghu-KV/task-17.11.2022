54817bf37a5a45515db6a4756aa410ccf941b611
Raghu-KV <raghunandanv19@gmail.com>
2022-11-21 14:27:21 +0530

    d

---
+++ src/components/PriceCard.jsx
@@ -0,0 +1,26 @@
+function PriceCard({ plan, price, content, notValid }) {
+  return (
+    <div className="col-4">
+      <div className="card bg-light p-3 my-border border-0 my-hover">
+        <div className="text-center border-bottom border-secondary">
+          <h6 className="text-secondary my-ltr">{plan}</h6>
+          <h2 className="display-4 font-weight-bold">
+            {price}
+            <span className="my-fontSize">/month</span>
+          </h2>
+        </div>
+        <div>
+          {content.map((ele) => (
+            <p className="pt-2 m-1">✔ {ele}</p>
+          ))}
+          {notValid &&
+            notValid.map((ele) => <p className="pt-2 m-1 my-dull">✖ {ele}</p>)}
+        </div>
+        <button className="btn btn-primary my-btn my-padding mt-2">
+          BUTTON
+        </button>
+      </div>
+    </div>
+  );
+}
+export default PriceCard;
